adda41c3f7f651bfe6797a5fffde8d25f0b457e0
Philou <philippe.bourgau@free.fr>
2010-09-10 07:36:50 +0200

    Add a test to ensure "<table>" is written in the cart html template (Per item shopping)

--- spec/views/cart/show.html.erb_spec.rb
+++ spec/views/cart/show.html.erb_spec.rb
@@ -6,15 +6,19 @@ describe "cart/show.html.erb" do
 
   before(:each) do
     assigns[:cart] = stub(Cart, :items => ["Tomates", "Pommes de terre"].map {|name| stub(Item, :name => name) } )
+    render
+  end
+
+  it "renders items in an html table" do
+    # I had problems because of ill parsed .html.erb template
+    response.should have_selector("table")
   end
 
   it "displays the first item" do
-    render
     response.should contain("Tomates")
   end
 
   it "displays the second item" do
-    render
     response.should contain("Pommes de terre")
   end
 
