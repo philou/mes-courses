4e62bb7519436aaa4396f1d2989a1642dda7dde3
Philou <philippe.bourgau@free.fr>
2010-12-24 11:13:42 +0100

    Create custom continuous integration environment (Infrastructure > Continuous integration)

--- config/environments/ci.rb
+++ config/environments/ci.rb
@@ -1,5 +1,9 @@
 # Copyright (C) 2010 by Philippe Bourgau
 
+# Configuration for continuous integration, mostly like production, but
+#  - with rspec and cucumber
+#  - without mongo logger
+
 # Settings specified here will take precedence over those in config/environment.rb
 
 # The production environment is meant for finished, "live" apps.
@@ -15,7 +19,7 @@ config.action_view.cache_template_loading            = true
 # config.log_level = :debug
 
 # Use a different logger for distributed setups
-CentralLogger::Initializer.initialize_deprecated_logger(config)
+# CentralLogger::Initializer.initialize_deprecated_logger(config)
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
@@ -29,4 +33,10 @@ CentralLogger::Initializer.initialize_deprecated_logger(config)
 # Enable threaded mode
 # config.threadsafe!
 
+# testing gems
+config.gem 'cucumber-rails',   :lib => false, :version => '>=0.3.2' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber-rails'))
+config.gem 'database_cleaner', :lib => false, :version => '>=0.5.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/database_cleaner'))
+config.gem 'webrat',           :lib => false, :version => '>=0.7.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
+config.gem 'rspec',            :lib => false, :version => '>=1.3.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
+config.gem 'rspec-rails',      :lib => false, :version => '>=1.3.2' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 
--- lib/tasks/ci.rake
+++ lib/tasks/ci.rake
@@ -1,5 +1,14 @@
 # Copyright (C) 2010 by Philippe Bourgau
 
-desc "Checks all specs and scenarios"
-task :behaviours => [:spec, :cucumber, :environment] do
+desc "Checks all specs, then drops, creates, and migrates the db, finaly runs all scenarios"
+task :behaviours => [:ci,
+                     :spec,
+                     'db:drop',
+                     'db:create',
+                     'db:migrate',
+                     :cucumber]
+
+desc "Sets rails in ci mode"
+task :ci do
+  ENV['RAILS_ENV'] = RAILS_ENV = 'ci'
 end
