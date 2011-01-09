241b9da4a89a1f2ba363cc5369e081827effa715
Philou <philippe.bourgau@free.fr>
2011-01-09 07:37:50 +0100

    Add all spike scripts to main repo (Infrastructure > Unique working repo)

---
+++ spikes/scrapping.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'mechanize'
+
+agent = Mechanize.new
+mainPage = agent.get('file:///home/philou/Code/Startup/Spikes/www.auchandirect.fr/frontoffice/index.html')
+
+mainPage.search('#carroussel > div a').each do |link|
+  puts link.content
+end
+
+# check in mechanize page code to know how to requests another way
+link = mainPage.link_with(:text => "Charcuterie")
+puts link.text
+puts agent.current_page.title
+
+link.click
+puts agent.current_page.title
+puts agent.current_page
