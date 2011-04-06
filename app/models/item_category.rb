7de0f82285fda3bd467fb94f11d41350f1e69ee4
Philou <philippe.bourgau@free.fr>
2011-04-06 07:04:03 +0200

    Use acts as tree for ItemCategory (Infrastructure)

--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -1,18 +1,9 @@
-# Copyright (C) 2010 by Philippe Bourgau
+# Copyright (C) 2010, 2011 by Philippe Bourgau
 
 # Hierarchichal category of items,
 class ItemCategory < ActiveRecord::Base
   has_many :items
-
-  # replace with acts_as_tree plugin
-  belongs_to :parent,
-             :class_name => "ItemCategory",
-             :foreign_key => "parent_id"
-  has_many :children,
-           :class_name => "ItemCategory",
-           :foreign_key => "parent_id",
-           :order => "name",
-           :dependent => :destroy
+  acts_as_tree :order => "name"
 
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => "parent_id"
