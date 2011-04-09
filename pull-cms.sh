2253c800f1a23262cd569aaefc530c9a4149039f
Philou <philippe.bourgau@free.fr>
2011-04-09 08:33:11 +0200

    Fix missing git add in pull and push cms (Infrastructure > Auto cms deployment scripts)

--- pull-cms.sh
+++ pull-cms.sh
@@ -3,8 +3,9 @@
 git checkout cms_branch &&
 RAILS_ENV=production heroku db:pull --app mes-courses-cms &&
 RAILS_ENV=production rake db:to_fs &&
+git add . &&
 git commit -m "Merge live cms db (Bloging)" &&
 git checkout master &&
 git merge -s subtree cms_branch &&
 
-echo "Cms pulled to master"
+echo "CMS pulled to master"
--- push-cms.sh
+++ push-cms.sh
@@ -3,9 +3,10 @@
 git checkout cms_branch &&
 git merge -s subtree cms_branch &&
 RAILS_ENV=production rake fs:to_db &&
+git add . &&
 git commit -m "Push local changes to cms db (Bloging)" &&
 git push cms cms_branch:master &&
 RAILS_ENV=production heroku db:push --app mes-courses-cms &&
 git checkout master &&
 
-echo "Master pushed to cms"
+echo "Master pushed to CMS"
