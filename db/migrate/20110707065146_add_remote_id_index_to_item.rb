d87e0c3fed44f2f99307c4410d178fb21aae7f6c
Philou <philippe.bourgau@free.fr>
2011-07-08 08:09:45 +0200

    Fix name of indexes in migrations (Item shoping > Import)

--- db/migrate/20110707065146_add_remote_id_index_to_item.rb
+++ db/migrate/20110707065146_add_remote_id_index_to_item.rb
@@ -1,6 +1,7 @@
 class AddRemoteIdIndexToItem < ActiveRecord::Migration
+
   def self.up
-    remove_index "items", :name => "altered_items_name_item_sub_type_id"
+    remove_index "items", :name => "items_name_item_sub_type_id"
     change_column "items", "remote_id", :integer, :null => false
     add_index "items", ["remote_id"], :name => "items_remote_id_index", :unique => true
   end
@@ -8,6 +9,7 @@ class AddRemoteIdIndexToItem < ActiveRecord::Migration
   def self.down
     remove_index "items", :name => "items_remote_id_index"
     change_column "items", "remote_id", :integer, :null => true
-    add_index "items", ["name", "item_category_id"], :name => "altered_items_name_item_sub_type_id", :unique => true
+    add_index "items", ["name", "item_category_id"], :name => "items_name_item_sub_type_id", :unique => true
   end
+
 end
