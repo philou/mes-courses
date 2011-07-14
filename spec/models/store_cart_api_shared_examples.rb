74e4b198f9ecfea87d9522a2b5861da17baf5566
Philou <philippe.bourgau@free.fr>
2011-07-14 09:04:01 +0200

    Make cart spec more robust by first searching an available item (Item shopping > Refactoring)

--- spec/models/store_cart_api_shared_examples.rb
+++ spec/models/store_cart_api_shared_examples.rb
@@ -20,7 +20,7 @@ shared_examples_for "Any StoreCartAPI" do
     end
 
     it "should set the cart value to 0 when emptying the cart" do
-      @api.set_item_quantity_in_cart(1, sample_item)
+      @api.set_item_quantity_in_cart(1, sample_item_id)
 
       @api.empty_the_cart
       @api.value_of_the_cart.should == 0
@@ -29,22 +29,22 @@ shared_examples_for "Any StoreCartAPI" do
     it "should set the cart value to something greater than 0 when adding items to the cart" do
       @api.empty_the_cart
 
-      @api.set_item_quantity_in_cart(1, sample_item)
+      @api.set_item_quantity_in_cart(1, sample_item_id)
       @api.value_of_the_cart.should >  0
     end
 
     it "should set the cart value to 3 times that of one item when adding 3 items" do
       @api.empty_the_cart
 
-      @api.set_item_quantity_in_cart(1, sample_item)
+      @api.set_item_quantity_in_cart(1, sample_item_id)
       item_price = @api.value_of_the_cart
 
-      @api.set_item_quantity_in_cart(3, sample_item)
+      @api.set_item_quantity_in_cart(3, sample_item_id)
       @api.value_of_the_cart.should == 3*item_price
     end
 
     it "should synchronize different sessions with logout login" do
-      @api.set_item_quantity_in_cart(1, sample_item)
+      @api.set_item_quantity_in_cart(1, sample_item_id)
 
       @store_cart_api.new(@store_cart_api.valid_login, @store_cart_api.valid_password).with_logout do |api2|
         api2.empty_the_cart
@@ -56,11 +56,11 @@ shared_examples_for "Any StoreCartAPI" do
       @api.value_of_the_cart.should == 0
     end
 
-    attr_reader :sample_item, :store_cart_api
+    attr_reader :sample_item_id, :store_cart_api
 
     before(:all) do
-      @sample_item = extract_sample_item
-      @sample_item.should_not be_nil
+      @sample_item_id = extract_sample_item
+      @sample_item_id.should_not be_nil
     end
 
     private
@@ -73,13 +73,20 @@ shared_examples_for "Any StoreCartAPI" do
       laits.should_not be_nil
 
       laits.items.each do |item|
-        attributes = item.attributes
-        if attributes[:price] != 0.0 && /Lait entier/ =~ attributes[:summary]
-          return attributes[:remote_id]
+        item_id = item.attributes[:remote_id]
+        if item_available?(item_id)
+          return item_id
         end
       end
 
       return nil
+    end
+
+    def item_available?(item_id)
+      @store_cart_api.new(@store_cart_api.valid_login, @store_cart_api.valid_password).with_logout do |api|
+        api.set_item_quantity_in_cart(1, item_id)
+        return 0 < api.value_of_the_cart
+      end
     end
 
     def milk_subcat(parent)
