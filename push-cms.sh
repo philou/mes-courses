448ac83a85143931b0aa8e3896a94d6067ffeb79
Philou <philippe.bourgau@free.fr>
2011-09-28 08:58:12 +0200

    Improved push and pull cms scripts (Productivity)

--- push-cms.sh
+++ push-cms.sh
@@ -4,7 +4,7 @@ git checkout cms_branch &&
 git merge -s subtree master &&
 RAILS_ENV=production rake fs:to_db &&
 git add . &&
-git commit -m "Push local changes to cms db (Bloging)" &&
+git commit -m "Push local changes to cms db (Bloging)" --allow-empty &&
 git push cms cms_branch:master &&
 RAILS_ENV=production heroku db:push --app mes-courses-cms &&
 git checkout master &&
