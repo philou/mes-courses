75a5d3b1567f9b7b5be0a898ecedae724a659b3b
Philou <philippe.bourgau@free.fr>
2011-10-26 07:26:20 +0200

    Rename branches and envs (Productivity > synchronous continuous integration)

--- push.sh
+++ push.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 echo "Pushing to main repository" &&
-git push origin master &&
+git push main master &&
 echo &&
 
-echo "Deploying to mes-courses-dev" &&
-git push dev master &&
-heroku rake db:migrate --app mes-courses-dev &&
+echo "Deploying to mes-courses-cart-tester" &&
+git push cart-tester master &&
+heroku rake db:migrate --app mes-courses-cart-tester &&
 echo &&
 
-echo "Deploying to mes-courses-watchdog" &&
-git push watchdog master &&
-heroku rake db:migrate --app mes-courses-watchdog &&
+echo "Deploying to mes-courses-integ" &&
+git push integ master &&
+heroku rake db:migrate --app mes-courses-integ &&
 echo &&
 
-echo "master branch pushed to origin, dev, and watchdog"
+echo "master branch pushed to main, integ and testers"
