0458784053bf518f297b235692122a983dd12357
Philou <philippe.bourgau@free.fr>
2011-10-26 12:32:24 +0200

    Add timing to the integration script (Productivity > Synchronous continuous integration)

---
+++ do_integrate.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Integrating latest developments (Press return to continue)" &&
+read something &&
+
+echo "Pulling latest developments" &&
+git pull dev master &&
+echo &&
+
+echo "Running integration script" &&
+rake behaviours &&
+echo &&
+
+echo "Deploying to test and integration environments" &&
+./push.sh &&
+echo &&
+
+echo "Integration successful !"
--- integrate.sh
+++ integrate.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-echo "Integrating latest developments (Press return to continue)" &&
-read something &&
-
-echo "Pulling latest developments" &&
-git pull dev master &&
-echo &&
-
-echo "Running integration script" &&
-rake behaviours &&
-echo &&
-
-echo "Deploying to test and integration environments" &&
-./push.sh &&
-echo &&
-
-echo "Integration successful !"
+time ./do_integrate.sh
