f72f43742a4f53d2833ead4a6be761771361be83
Philou <philippe.bourgau@free.fr>
2012-06-29 07:15:42 +0200

    Refactor item search tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -52,67 +52,41 @@ describe Item do
   context "when searching items by keyword" do
 
     before :each do
-      @tomates = Item.new(:name => "Tomates")
-      @tomates_cerises = Item.new(:name => "Tomates cerises")
-      @concombres = Item.new(:name => "Concombres")
+      @marche = FactoryGirl.build_stubbed(:item_category, name: "Marché", parent: ItemCategory.root)
 
-      @legumes = ItemCategory.new(:name => "Légumes", :id => 11, :items => [@tomates, @tomates_cerises, @concombres])
+      @legumes = FactoryGirl.build_stubbed(:item_category, name: "Légumes", parent: @marche)
+      @fruits = FactoryGirl.build_stubbed(:item_category, name: "Fruits", parent: @marche)
+      @marche.stub(:children).and_return([@legumes, @fruits])
 
-      @cerises = Item.new(:name => "Cerises")
-      @abricots = Item.new(:name => "Abricots")
-
-      @fruits = ItemCategory.new(:name => "Fruits", :id => 22, :items => [@cerises, @abricots])
-
-      @marche = ItemCategory.new(:name => "Marché", :id => 33, :children => [@legumes, @fruits])
-      @marche.children.each { |child| child.parent = @marche}
-
-      @tomates_confites = Item.new(:name => "Tomates confites")
-      @salade_cesar = Item.new(:name => "Salade césar", :summary => "Salade préparée, antipastis et poulet pané")
-
-      @italien = ItemCategory.new(:name => "Italien", :id => 44, :items => [@tomates_confites, @salade_cesar])
-
-      @traiteur = ItemCategory.new(:name => "Traiteur", :id => 55, :children => [@italien])
-      @traiteur.children.each { |child| child.parent = @traiteur}
-
-      @root_item_category = ItemCategory.new(:name => ItemCategory.root_name, :id => 66, :children => [@marche, @traiteur])
-      @root_item_category.children.each { |child| child.parent = @root_item_category}
+      @expected = stub("Array of items")
     end
 
     it "should directly search items when it has no children" do
-      keyword = "tomates"
-      expected = [@tomates, @tomates_cerises]
-
       Item.should_receive(:where).
         with(/item_category_id = :category_id/, hash_including(:category_id => @legumes.id)).
-        and_return(join_mock(expected))
+        and_return(join_mock)
 
-      Item.search_by_string_and_category(keyword, @legumes).should == expected
+      Item.search_by_string_and_category("tomates", @legumes).should == @expected
     end
 
-    def join_mock(result)
+    def join_mock()
       mock = stub("Where clause")
-      mock.stub(:joins).with("INNER JOIN item_categories_items ON (items.id = item_categories_items.item_id)").and_return(result)
+      mock.stub(:joins).with("INNER JOIN item_categories_items ON (items.id = item_categories_items.item_id)").and_return(@expected)
       mock
     end
 
     it "should search in sub categories when it has no parent" do
-      keyword = "cerises"
-      expected = [@tomates_cerises, @cerises]
-
       Item.should_receive(:where).
         with(/item_category_id in \(:category_ids\)/, hash_including(:category_ids => [@legumes.id,@fruits.id])).
-        and_return(join_mock(expected))
+        and_return(join_mock)
 
-      Item.search_by_string_and_category(keyword, @marche).should == expected
+      Item.search_by_string_and_category("cerise", @marche).should == @expected
     end
 
     it "should search all items when root category is specified" do
-      keyword = "tomates"
-      expected = [@tomates, @tomates_cerises, @tomates_confites]
+      Item.should_receive(:where).and_return(join_mock)
 
-      Item.should_receive(:where).and_return(join_mock(expected))
-
-      Item.search_by_string_and_category(keyword, @root_item_category).should == expected
+      Item.search_by_string_and_category("tomates", ItemCategory.root).should == @expected
     end
 
     it "should search in tokens column" do
@@ -121,11 +95,11 @@ describe Item do
       Item.should_receive(:where).
         with(/items.tokens like :token0/, hash_including(:token0 => "%#{search_string}%")).
         exactly(3).times.
-        and_return(join_mock([@salade_cesar]))
+        and_return(join_mock)
 
-      Item.search_by_string_and_category(search_string, @root_item_category)
-      Item.search_by_string_and_category(search_string, @traiteur)
-      Item.search_by_string_and_category(search_string, @italien)
+      Item.search_by_string_and_category(search_string, ItemCategory.root)
+      Item.search_by_string_and_category(search_string, @marche)
+      Item.search_by_string_and_category(search_string, @legumes)
     end
 
     it "should search every tokens in the search string" do
@@ -136,11 +110,11 @@ describe Item do
       Item.should_receive(:where).
         with(/items.tokens like :token0 and items.tokens like :token1/, hash_including(:token0 => "%#{tokens[0]}%", :token1 => "%#{tokens[1]}%")).
         exactly(3).times.
-        and_return(join_mock([@salade_cesar]))
+        and_return(join_mock)
 
-      Item.search_by_string_and_category(search_string, @root_item_category)
-      Item.search_by_string_and_category(search_string, @traiteur)
-      Item.search_by_string_and_category(search_string, @italien)
+      Item.search_by_string_and_category(search_string, ItemCategory.root)
+      Item.search_by_string_and_category(search_string, @marche)
+      Item.search_by_string_and_category(search_string, @legumes)
     end
   end
 end
