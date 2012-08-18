16e54d632e9cb21a869a54d62e1a2b1374d8c3ca
Philou <philippe.bourgau@free.fr>
2012-08-18 08:05:40 +0200

    Fix stores rake tasks

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,6 @@
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-# Load lib files
-require_relative "../lib/mes_courses"
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
