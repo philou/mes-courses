fcbe4e3b6ebd9e441b4c94c8b9a2daa46e37d857
Philou <philippe.bourgau@free.fr>
2012-08-18 07:34:30 +0200

    Fix integrate script

--- lib/mes_courses/utils/timing.rb
+++ lib/mes_courses/utils/timing.rb
@@ -2,25 +2,26 @@
 # Copyright (C) 2011, 2012 by Philippe Bourgau
 
 # Provides a timer to a block of code while executing
-module MesCourses::Utils
-  class Timing
-    def self.duration_of
-      yield new
-    end
+module MesCourses
+  module Utils
+    class Timing
+      def self.duration_of
+        yield new
+      end
 
-    def self.now
-      Time.now
-    end
+      def self.now
+        Time.now
+      end
 
-    def seconds
-      Timing.now - @t0
-    end
+      def seconds
+        Timing.now - @t0
+      end
 
-    private
+      private
 
-    def initialize
-      @t0 = Timing.now
+      def initialize
+        @t0 = Timing.now
+      end
     end
   end
 end
-
