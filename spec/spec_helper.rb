2a84f8c6569ba63caf124bb5bfb145e7e56edae2
Philou <philippe.bourgau@free.fr>
2012-08-29 07:22:06 +0200

    Spec helper multiple load guard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,31 @@
 # -*- encoding: utf-8 -*-
 # Copyright (C) 2010, 2012 by Philippe Bourgau
 
+puts $LOADED_FEATURES
+
+# figure out where we are being loaded from
+if $LOADED_FEATURES.grep(/spec\/spec_helper\.rb/).any?
+  begin
+    raise "foo"
+  rescue => e
+    puts <<-MSG
+  ===================================================
+  It looks like spec_helper.rb has been loaded
+  multiple times. Normalize the require to:
+
+    require "spec/spec_helper"
+
+  Things like File.join and File.expand_path will
+  cause it to be loaded multiple times.
+
+  Loaded this time from:
+
+    #{e.backtrace.join("\n    ")}
+  ===================================================
+    MSG
+  end
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
