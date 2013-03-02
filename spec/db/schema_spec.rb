a929e2de24d63217b80ec6b7213597546fabccad
Philou <philippe.bourgau@free.fr>
2013-03-02 07:30:20 +0100

    Change IO.lines to IO.each_line

--- spec/db/schema_spec.rb
+++ spec/db/schema_spec.rb
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-# Copyright (C) 2011, 2012 by Philippe Bourgau
+# Copyright (C) 2011, 2012, 2013 by Philippe Bourgau
 
 require "spec_helper"
 
@@ -8,7 +8,7 @@ describe "Schema" do
   it "uses infinite text columns instead of bounded string columns" do
     File.open(File.join(Rails.root, 'db', 'schema.rb'), "r") do |schema_file|
 
-      lines = schema_file.lines
+      lines = schema_file.each_line
       lines = lines.find_all {|line| line =~ /[^a-zA-Z0-9_]string[^a-zA-Z0-9_]/ } # get all lines with the word "string"
       lines = lines.find_all {|line| !(line =~ /t\.string\s+\"[^\"]*_type\"/)} # ignore polymorphic xxx_type columns
 
