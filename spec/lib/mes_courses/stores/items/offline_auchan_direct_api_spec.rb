8dfe814f04b8ae63d173435590a65717e074f27e
Philou <philippe.bourgau@free.fr>
2013-04-12 06:52:51 +0200

    Make auchandirect prix promo offline test more robust

--- spec/lib/mes_courses/stores/items/offline_auchan_direct_api_spec.rb
+++ spec/lib/mes_courses/stores/items/offline_auchan_direct_api_spec.rb
@@ -3,6 +3,7 @@
 
 require 'spec_helper'
 require_relative 'api_shared_examples'
+require 'lazing'
 
 module MesCourses
   module Stores
@@ -18,13 +19,62 @@ module MesCourses
         it_should_behave_like_any_store_items_api
 
         it "parses promotions prices" do
-          cat1 = @store.categories.find {|cat| cat.title =~ /Traiteur/}
-          cat2 = cat1.categories.find {|cat| cat.title =~ /Traiteur/}
-          cat3 = cat2.categories.find {|cat| cat.title =~ /Apéritifs/}
-          item = cat3.items.find {|item| item.title =~ /APERICUBE/}
-          item.attributes[:price].should be_instance_of(Float)
+          enumerator_of_cat_sub_cat_and_items_tokens.
+
+          mapping do |tokens|
+            promo_price(*tokens)
+          end.
+
+          selecting do |price|
+            !price.nil?
+          end.
+
+          first.should be_instance_of(Float)
         end
 
+        private
+
+        def enumerator_of_cat_sub_cat_and_items_tokens
+          `find #{File.join(Rails.root,'offline_sites','www.auchandirect.fr')} -name *.html -exec grep -l "prix-promo" {} \\;`.split("\n").
+
+          selecting do |file|
+            doc = Nokogiri::HTML(open(file))
+            doc.search("#produit-infos .bloc-prix-promo > span.prix-promo").any?
+          end.
+
+          mapping do |file|
+            file = URI.unescape(file)
+            file = file.gsub(/^.*\/www\.auchandirect\.fr\//, "")
+            pieces = file.split("/").take(3)
+            pieces[2] = pieces[2].split(",")[0]
+            pieces = pieces.map do |piece|
+              piece.gsub(/lv_/,'').split(/[\-_, \(\)']+/).select {|p| not (p.size <= 2 or p =~ /^[0-9]/)}
+            end
+
+            pieces
+          end
+        end
+
+        def promo_price(cat_tokens, sub_cat_tokens, item_tokens)
+          filter(@store.categories, cat_tokens).each do |cat|
+            filter(cat.categories, sub_cat_tokens).each do |sub_cat|
+              sub_cat.categories.each do |sub_sub_cat|
+                filter(sub_sub_cat.items, item_tokens).each do |item|
+                  return item.attributes[:price]
+                end
+              end
+            end
+          end
+          nil
+        end
+
+        def filter(sub_elements, tokens)
+          sub_elements.select do |element|
+            tokens.all? do |token|
+              element.attributes[:name].downcase.include?(token.downcase)
+            end
+          end
+        end
       end
     end
   end
