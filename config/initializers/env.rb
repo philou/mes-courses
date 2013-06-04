18dbd485470d4406f96f6c3f8b0744022f77d48c
Philou <philippe.bourgau@free.fr>
2013-06-04 09:20:54 +0200

    Fail fast if encryption key is missing

--- config/initializers/env.rb
+++ config/initializers/env.rb
@@ -4,3 +4,5 @@ env_file = File.join(Rails.root, 'config', 'local_env.yml')
 YAML.load(File.open(env_file)).each do |key, value|
   ENV[key.to_s] = value
 end if File.exists?(env_file)
+
+raise SecurityError.new("The ENCRYPTION_KEY envrionment variable should be specified") if ENV["ENCRYPTION_KEY"].nil?
