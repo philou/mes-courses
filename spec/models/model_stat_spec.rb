96116e1844ef6ef003c1fe7c50c34501e24b4ea4
Philou <philippe.bourgau@free.fr>
2013-10-14 07:02:40 +0200

    Remove mocks in ModelStat specs

--- spec/models/model_stat_spec.rb
+++ spec/models/model_stat_spec.rb
@@ -5,40 +5,54 @@ require 'spec_helper'
 
 describe ModelStat do
 
+  ROOT_COUNT = 2 # root and disabled
+  CATEGORY_COUNT = 4
+  ITEM_COUNT = 20
+
   before(:each) do
-    @stats = { ModelStat::ROOT_CATEGORY => {:old_count => 1, :count => 2, :delta => 2.0},
-               ModelStat::CATEGORY => {:old_count => 4, :count => 8, :delta => 2.0},
-               ModelStat::ITEM => {:old_count => 20, :count => 27, :delta => 1.35}}
+    # root categories
+    ItemCategory.root
+    ItemCategory.disabled
 
-    Item.stub(:count).        and_return(@stats[ModelStat::ITEM][:count])
-    ItemCategory.stub(:count).and_return(@stats[ModelStat::ROOT_CATEGORY][:count],
-                                         @stats[ModelStat::CATEGORY][:count])
+    @a_category = create_category
+    ITEM_COUNT.times { create_item }
+
+    while ItemCategory.count < CATEGORY_COUNT
+      create_category
+    end
   end
 
-  it "should save item stats when updating" do
-    ModelStat::ALL.each do |name|
-      stat = ModelStat.new(:name => name)
-      expect(ModelStat).to receive(:find_or_initialize_by_name).with(name).and_return(stat)
-      expect(stat).to receive(:update_attributes!).with(:count => @stats[name][:count])
-    end
+  def create_category
+    FactoryGirl.create(:item_category, parent: ItemCategory.root)
+  end
+  def create_item
+    FactoryGirl.create(:item, item_categories: [@a_category])
+  end
 
+  it "should snapshot current counts when updating" do
     ModelStat.update!
+
+    expect(ModelStat.find_by_name(ModelStat::ITEM).count).to eq(ITEM_COUNT)
+    expect(ModelStat.find_by_name(ModelStat::CATEGORY).count).to eq(CATEGORY_COUNT)
+    expect(ModelStat.find_by_name(ModelStat::ROOT_CATEGORY).count).to eq(ROOT_COUNT)
   end
 
-  it "should generate the expected delta" do
-    old_stats = @stats.map do |model, stat|
-      ModelStat.new(:name => model, :count => stat[:old_count])
-    end
-    ModelStat.stub(:all).and_return(old_stats)
+  it "should generate the expected delta from curent count versus saved model stats" do
+    ModelStat.update!
 
-    expect(ModelStat.generate_delta).to eq @stats
+    (ITEM_DELTA = 7).times { create_item }
+    (CATEGORY_DELTA = 4).times { create_category }
+
+    stats = ModelStat.generate_delta
+    expect(stats[ModelStat::ROOT_CATEGORY]).to eq(old_count: ROOT_COUNT, count: ROOT_COUNT, delta: 1.0)
+    expect(stats[ModelStat::CATEGORY]).to eq(old_count: CATEGORY_COUNT, count: CATEGORY_COUNT + CATEGORY_DELTA, delta: (CATEGORY_COUNT + CATEGORY_DELTA).to_f / CATEGORY_COUNT)
+    expect(stats[ModelStat::ITEM]).to eq(old_count: ITEM_COUNT, count: ITEM_COUNT + ITEM_DELTA, delta: (ITEM_COUNT + ITEM_DELTA).to_f / ITEM_COUNT)
   end
 
   it "should generate delta without previous stats should have old_count == 0" do
-    ModelStat.stub(:all).and_return([])
-
     ModelStat.generate_delta.each do |name, stats|
       expect(stats[:old_count]).to eq 0
+      expect(stats[:delta]).to be_nil
     end
   end
 
