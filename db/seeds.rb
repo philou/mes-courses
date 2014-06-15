988b9c52f76d02c5f5a07adeb2c2c7e35111e590
Philou <philippe.bourgau@free.fr>
2014-06-15 21:10:28 +0200

    Add AGPL header to .rb and .coffee files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
 # -*- encoding: utf-8 -*-
 # Copyright (C) 2010, 2011, 2012 by Philippe Bourgau
+# http://philippe.bourgau.net
+
+# This file is part of mes-courses.
+
+# mes-courses is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
